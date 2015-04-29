5dabfa929760c3f774262213fbc90e2e397c4942
jocelyn alsdorf & Alex Caste <student@epicodus.com>
2015-04-29 10:06:27 -0700

    adding initial file structure. writingfirst spec to return car make.  writing method to save make, model, year.  writing method to return make. first spec passing

---
+++ lib/vehicle.rb
@@ -0,0 +1,29 @@
+class Vehicle
+  @@vehicles_list = []
+
+  define_method(:initialize) do |make, model, year|
+    @make = make
+    @model = model
+    @year = year
+    # @id = @@vehicles_list.length().+(1)
+  end
+
+  define_method(:make) do
+    @make
+  end
+
+  define_method(:model) do
+    @model
+  end
+
+  define_method(:year) do
+    @year
+  end
+
+  define_method(:save) do
+    @@vehicles_list.push(self)
+  end
+
+
+
+end

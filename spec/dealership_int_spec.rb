9bc9004bccc6d3360d44ca8ad25d7a211950bc9b
jocelyn alsdorf & Alex Caste <student@epicodus.com>
2015-04-29 13:51:57 -0700

    Writing passing spec to test adding new vehicle to dealership path

--- spec/dealership_int_spec.rb
+++ spec/dealership_int_spec.rb
@@ -25,4 +25,35 @@ describe('path for index', {:type => :feature}) do
     end
   end
 
+  describe('path for all adding and showing all dealerships', {:type => :feature}) do
+    it('shows the list of all available ') do
+      visit('/all_dealers/new')
+      fill_in('name', :with => 'Manny')
+      click_button('Add Dealership')
+      expect(page).to have_content('Success')
+      click_link('See Dealership List')
+      expect(page).to have_content('Dealerships')
+      click_link("Manny")
+      expect(page).to have_content('Here are')
+    end
+  end
+
+  describe('path for all adding a vehicle to a dealership', {:type => :feature}) do
+    it('shows the list of all available ') do
+      visit('/all_dealers/new')
+      fill_in('name', :with => 'Killian')
+      click_button('Add Dealership')
+      expect(page).to have_content('Success')
+      click_link('See Dealership List')
+      expect(page).to have_content('Dealerships')
+      click_link("Killian")
+      expect(page).to have_content('Here are')
+      click_link("Add a new vehicle")
+      fill_in('make', :with => 'Honda')
+      fill_in('model', :with => 'Accord')
+      fill_in('year', :with => '1998')
+      click_button('Add Vehicle')
+      expect(page).to have_content('Success')
+    end
+  end
 end

33b65dc1c8a5469bf9f1886872f50f6dd72e8a6a
jocelyn alsdorf & Alex Caste <student@epicodus.com>
2015-04-29 13:04:59 -0700

    Writing spec to test adding a vehicle to a dealership

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -59,4 +59,13 @@ describe(Dealership) do
       expect(Dealership.find(test_dealership.id())).to(eq(test_dealership))
     end
   end
+
+  describe("#add_vehicle") do
+    it("adds a new vehicle to a dealership") do
+      test_dealership = Dealership.new("Bob's Cars")
+      test_vehicle = Vehicle.new("Mini", "Cooper", 2000)
+      test_dealership.add_vehicle(test_vehicle)
+      expect(test_dealership.cars()).to(eq([test_vehicle]))
+    end
+  end
 end

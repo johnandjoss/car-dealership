0f54994834f068cc9254e30159e89afa7432969d
jocelyn alsdorf & Alex Caste <student@epicodus.com>
2015-04-29 10:09:22 -0700

    Writing second and third spec to test return car model and year.  tests are passing.

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -11,4 +11,20 @@ describe(Vehicle) do
       expect(test_vehicle.make()).to(eq("Toyota"))
     end
   end
+
+  describe("#model") do
+    it("returns the model of a vehicle") do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_vehicle.save()
+      expect(test_vehicle.model()).to(eq("Prius"))
+    end
+  end
+
+  describe("#year") do
+    it("returns the model of a vehicle") do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_vehicle.save()
+      expect(test_vehicle.year()).to(eq(2000))
+    end
+  end
 end

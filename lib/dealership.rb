79253e7d85a02ee30c954b7e4e372668f81700c6
jocelyn alsdorf & Alex Caste <student@epicodus.com>
2015-04-29 11:49:15 -0700

    Writing passing method to return dealership name:

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,4 +1,13 @@
 class Dealership
+  @@dealerships = []
 
-  
+  define_method(:initialize) do |name|
+    @name = name
+    @id = @@dealerships.length().+(1)
+    @cars = []
+  end
+
+  define_method(:name) do
+    @name
+  end
 end

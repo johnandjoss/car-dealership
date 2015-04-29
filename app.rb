29de87fa757c3d185941dfa9d0f61e0171edcde5
jocelyn alsdorf & Alex Caste <student@epicodus.com>
2015-04-29 13:44:05 -0700

    Writing passing spec to input new dealerships

--- app.rb
+++ app.rb
@@ -9,10 +9,26 @@ get('/') do
   erb(:index)
 end
 
-get('/dealerships') do
+get('/all_dealers/') do
   @dealerships = Dealership.all()
+  erb(:all_dealers)
 end
 
+get('/all_dealers/new') do
+  erb(:dealerships_form)
+end
+
+get('/all_dealers') do
+  @dealerships = Dealership.all()
+  erb(:all_dealers)
+end
+
+post('/all_dealers') do
+  name = params.fetch('name')
+  Dealership.new(name).save()
+  @dealerships = Dealership.all()
+  erb(:success_deal)
+end
 
 get('/all_vehicles') do
   @vehicles = Vehicle.all()
@@ -35,4 +51,25 @@ end
 get('/all_vehicles/:id') do
   @vehicle = Vehicle.find(params.fetch("id"))
   erb(:vehicle)
+end
+
+get('/all_dealers/:id') do
+  @dealership = Dealership.find(params.fetch('id').to_i())
+  erb(:dealership)
+end
+
+get('/all_dealers/:id/vehicles/new') do
+  @dealership = Dealership.find(params.fetch('id').to_i())
+  erb(:dealership_vehicle_form)
+end
+
+post('/vehicles') do
+  make = params.fetch('make')
+  model = params.fetch('model')
+  year = params.fetch('year')
+  @vehicle = Vehicle.new(make, model, year)
+  @vehicle.save()
+  @dealership = Dealership.find(params.fetch('dealership_id').to_i())
+  @dealership.add_vehicle(@vehicle)
+  erb(:success)
 end
--- spec/dealership_int_spec.rb
+++ spec/dealership_int_spec.rb
@@ -8,4 +8,21 @@ describe('path for index', {:type => :feature}) do
     visit('/')
     expect(page).to have_content('Welcome to')
   end
+
+  describe('path for all dealerships', {:type => :feature}) do
+    it('shows the list of all dealerships ') do
+      visit('/all_dealers')
+      expect(page).to have_content('Dealerships')
+    end
+  end
+
+  describe('path for all dealers/new', {:type => :feature}) do
+    it('allows user to input a new dealership ') do
+      visit('/all_dealers/new')
+      fill_in('name', :with => 'Ford')
+      click_button('Add Dealership')
+      expect(page).to have_content('Success')
+    end
+  end
+
 end

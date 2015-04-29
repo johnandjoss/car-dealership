70d389ea4e76da99f04cc9c1bfa5c7c817421dcc
jocelyn alsdorf & Alex Caste <student@epicodus.com>
2015-04-29 11:15:13 -0700

    creates path for seeing all the available cars as well as the path for adding a vehicle; capybara tests for these paths passes

--- app.rb
+++ app.rb
@@ -7,3 +7,21 @@ require('./lib/vehicle')
 get('/') do
   erb(:index)
 end
+
+get('/all_vehicles') do
+  @vehicles = Vehicle.all()
+  erb(:all_vehicles)
+end
+
+get('/all_vehicles/new') do
+  erb(:vehicle_form)
+end
+
+post('/all_vehicles') do
+  make = params.fetch("make")
+  model = params.fetch("model")
+  year = params.fetch("year")
+  vehicle = Vehicle.new(make, model, year)
+  vehicle.save()
+  erb(:success)
+end
--- spec/vehicle_int_spec.rb
+++ spec/vehicle_int_spec.rb
@@ -3,9 +3,27 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('path for index to return a vehicle list', {:type => :feature}) do
-  it('shows the list of cars and shows link to "add car" form') do
+describe('path for index', {:type => :feature}) do
+  it('shows the link to list of cars and "add car" form') do
     visit('/')
-    expect(page).to have_content('Welcome to Vehicle saver')
+    expect(page).to have_content('Welcome to')
+  end
+end
+
+describe('path for all vehicles', {:type => :feature}) do
+  it('shows the list of all available ') do
+    visit('/all_vehicles')
+    expect(page).to have_content('Here are')
+  end
+end
+
+describe('path for all vehicles/new', {:type => :feature}) do
+  it('allows user to input a new vehicle ') do
+    visit('/all_vehicles/new')
+    fill_in('make', :with => 'Ford')
+    fill_in('model', :with => 'Mustang')
+    fill_in('year', :with => '1999')
+    click_button('Add Vehicle')
+    expect(page).to have_content('Success')
   end
 end
